f0a622d82bca8cdf69b7d2038d04953cdcfde35e
Piyush Kumar Singh <piyushkumarsingh665@gmail.com>
2025-09-29 22:37:13 +0530

    Initial Commit with Auth Form

---
+++ components/FromField.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+
+interface FormFieldProps<T extends FieldValues>{
+    control: Control<T>,
+    name: Path<T>,
+    label: string,
+    placeholder: string,
+    type: 'text' | 'email' | 'password' |'file'
+
+}
+
+const FromField = <T extends FieldValues>({control, name, label, placeholder, type="text"}:FormFieldProps<T>) => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input className="input" placeholder={placeholder}{...field}  type={type}/>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default FromField;
