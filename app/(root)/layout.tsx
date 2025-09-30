71eb021cc01e879016da4ea14e732ba2478ba2d2
Piyush Kumar Singh <piyushkumarsingh665@gmail.com>
2025-09-30 15:27:22 +0530

    Home Page UI completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({children}:{children: ReactNode}) => {
   return (
-    <div>
+    <div className='root-layout'>
+      <nav>
+        <Link href={"/"} className='flex items-center gap-2'>
+          <Image alt='logo' src={"/logo.svg"} height={32} width={38}/>
+          <h2 className="text-primary-100">
+            IntelliView
+          </h2>
+        </Link>
+      </nav>
       {children}
+      
     </div>
   )
 }
---
+++ components/DisplayTechIcons.tsx
@@ -0,0 +1,31 @@
+import { getTechLogos } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
+  const techIcons = await getTechLogos(techStack);
+
+  return (
+    <div className="flex flex-row">
+      {techIcons.slice(0, 3).map(({ tech, url }, idx) => (
+        <div
+          key={tech}
+          className={`relative group bg-dark-300 rounded-full p-2 flex-center ${
+            idx >= 1 && "-ml-3"
+          }`}
+        >
+          <span className="tech-tooltip">{tech}</span>
+          <Image
+            src={url}
+            alt="tech"
+            height={100}
+            width={100}
+            className="size-5"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DisplayTechIcons;
