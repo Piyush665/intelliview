42eb637367c4c3811bb0b32f9bbc5b37f8fc50ae
Piyush Kumar Singh <piyushkumarsingh665@gmail.com>
2025-10-01 14:48:01 +0530

    Implemented Interview Generation

--- lib/utils.ts
+++ lib/utils.ts
@@ -46,3 +46,5 @@ export const getRandomInterviewCover = () => {
   const randomIndex = Math.floor(Math.random() * interviewCovers.length);
   return `/covers${interviewCovers[randomIndex]}`;
 };
+
+
